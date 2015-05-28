06f02390496aeec88ead8a81c0ef445a0bd0467f
compulim <compulim@hotmail.com>
2015-05-28 10:54:09 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,79 @@
+!function (UglifyJS, path, util) {
+    'use strict';
+
+    var number = util.number,
+        replaceMultiple = util.regexp.replaceMultiple,
+        time = util.time;
+
+    module.exports = function (inputs, outputs, callback) {
+        var that = this;
+
+        inputs = inputs.newOrChanged;
+
+        Object.getOwnPropertyNames(inputs).forEach(function (filename) {
+            var startTime = Date.now(),
+                original = inputs[filename],
+                minified;
+
+            if ((path.extname(filename) || '').toLowerCase() === '.html') {
+                minified = outputs[filename] = new Buffer(processHTML(original.toString()));
+            } else {
+                var uglified = UglifyJS.minify(original.toString(), { fromString: true });
+
+                minified = outputs[filename] = uglified.code;
+
+                if (uglified.map) {
+                    outputs[filename + '.map'] = uglified.map;
+                }
+            }
+
+            that.log([
+                'Uglified ',
+                filename,
+                ', took ',
+                time.humanize(Date.now() - startTime),
+                ' (',
+                number.bytes(original.length),
+                ' -> ',
+                number.bytes(minified.length),
+                ', ',
+                (((minified.length / original.length) - 1) * 100).toFixed(1),
+                '%)'
+            ].join(''));
+        });
+
+        callback(null, outputs);
+    };
+
+    function processFile(filename, buffer) {
+        if ((path.extname(filename) || '').toLowerCase() === '.html') {
+            return processHTML(buffer);
+        } else  {
+            return processJavaScript(buffer);
+        }
+    }
+
+    function processHTML(text) {
+        var pattern = /(<script [^>]*?type="text\/javascript"[^>]*>)([\s\S]*?)(<\/script>)/gmi;
+
+        return replaceMultiple(
+            text,
+            [
+                [
+                    /(<script [^>]*?type="text\/javascript"[^>]*>)([\s\S]*?)(<\/script>)/gmi,
+                    function (match0, match1, match2, match3, index, input) {
+                        return match1 + UglifyJS.minify(match2, { fromString: true }).code + match3;
+                    }
+                ]
+            ]
+        );
+    }
+
+    function processJavaScript(code) {
+        return UglifyJS.minify(code, { fromString: true });
+    }
+}(
+    require('uglify-js'),
+    require('path'),
+    require('publishjs').util
+);
---
+++ src/test/integration-test.js
@@ -0,0 +1,33 @@
+!function (assert, path) {
+    'use strict';
+
+    require('vows').describe('Integration test').addBatch({
+        'When minifying a CSS file': {
+            topic: function () {
+                var callback = this.callback,
+                    topic;
+
+                require('publishjs')({
+                    cache: false,
+                    log: false,
+                    processors: {
+                        cssmin: require('../index')
+                    }
+                }).build(function (pipe, callback) {
+                    pipe.from(path.resolve(path.dirname(module.filename), 'integration-test-files'))
+                        .cssmin()
+                        .run(callback);
+                }, callback);
+            },
+
+            'should returns a minified copy': function (topic) {
+                assert.equal(Object.getOwnPropertyNames(topic).length, 2);
+                assert.equal(topic['index.js'].toString(), '!function(){"use strict";console.log("Hello, World!")}();');
+                assert.equal(topic['index.html'].toString().replace(/\r/g, ''), '<!DOCTYPE html>\n<html lang="en-US">\n<head>\n    <script type="text/javascript">!function(){"use strict";console.log("Hello, World!")}();</script>\n</head>\n</html>');
+            }
+        }
+    }).export(module);
+}(
+    require('assert'),
+    require('path')
+);
